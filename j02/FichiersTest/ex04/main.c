3be3c46691069898b2b193b9997bde7c7c7e1209
Louis-philippe RIBERE <lribere@e2r9p18.42.fr>
2017-08-31 21:54:16 +0200

    Ex02

--- j02/FichiersTest/ex04/main.c
+++ j02/FichiersTest/ex04/main.c
@@ -6,17 +6,18 @@
 /*   By: lribere <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/08/31 12:23:31 by lribere           #+#    #+#             */
-/*   Updated: 2017/08/31 20:24:18 by lribere          ###   ########.fr       */
+/*   Updated: 2017/08/31 21:53:18 by lribere          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-#include <unistd.h>
-int     ft_putchar(char c);
-void    ft_is_negative(void);
 
-int		main(void)
-{	
-	int n;
-	
-	ft_is_negative();
+#include <unistd.h>
+
+int		ft_putchar(char c);
+void	ft_is_negative(int n);
+
+int		main()
+{
+
+	ft_is_negative(-14);
 	return (0);
 }
